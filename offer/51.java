818b74f3a284217519714312f8cdb59e93da4cb4
galaxyenginneer <47632375+galaxyenginneer@users.noreply.github.com>
2020-02-28 16:00:39 +0800

    51

--- offer/51.java
+++ offer/51.java
@@ -1,27 +1,47 @@
 import java.util.*;
 class Solution {
-    // List<Integer> list;
-    Node head=new Node(Integer.MIN_VALUE);
-    // Node tail=head;
-    // int num=1;
-    int incert(int val){
-        int i=0;
-        Node p=head;
-        for (;p.next!=null;i++){
-            if(p.next.val>val)break;
-            p=p.next;
+    int res=0;
+    int[] nums;
+    int[] merge(int[]a, int[]b){
+        int[] sum=new int[a.length+b.length];
+        int i=0,j=0,k=0;
+        while(k<sum.length){
+            if (i==a.length){
+                for(;j<b.length;j++,k++){
+                    sum[k]=b[j];
+                }
+                break;
+            }
+            if (j==b.length){
+                for(;i<a.length;i++,k++){
+                    sum[k]=a[i];
+                }
+                break;
+            }
+            if (a[i]<=b[j]){
+                sum[k]=a[i];
+                i++;
+            }else{
+                sum[k]=b[j];
+                j++;
+                res+=a.length-i;
+            }
+            k++;
         }
-        p.next=new Node(val);
-        return i;
+        return sum;
+    }
+    int[] mergesort(int a,int b){
+        if (a==b) return new  int[]{nums[a]};
+        int mid=(a+b)/2;
+        return merge(mergesort(a,mid),mergesort(mid+1, b));
     }
     public int reversePairs(int[] nums) {
-        int res=0;
+        // int res=0;
+        this.nums=nums;
         // list=new LinkedList<Integer>();
         if (nums.length==0) return 0;
         // list.add(nums[nums.length-1]);
-        for (int i=nums.length-2;i>-1;i--){
-            res+=incert(nums[i]);
-        }
+        mergesort(0, nums.length-1);
         return res;
     }
     class Node{
